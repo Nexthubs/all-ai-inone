59e56259ff89b5ad3cd707c0c29838e83761af47
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2025-04-13 18:47:56 +0800

    Update realtime.vue

--- src/views/wav/realtime.vue
+++ src/views/wav/realtime.vue
@@ -1,12 +1,17 @@
-<script setup lang="ts">
-</script>
+<script lang="ts" setup>
+import { ref ,watch} from 'vue';
+import realtime from './realtime.vue';
+import { homeStore } from '@/store';
+import { mlog } from '@/api';
+const st= ref({ isRealtime:false })
 
+watch(()=>homeStore.myData.act,(n)=>{
+    mlog('isRealtime',n)
+    if(n=='openRealtime'){
+        st.value.isRealtime=true
+    }
+})
+</script>
 <template>
-  <div class="w-full h-full">
-    <iframe 
-      src="https://flow.nexthubs.cn/workflow" 
-      frameborder="0" 
-      class="w-full h-full"
-    ></iframe>
-  </div>
+    <realtime v-if="st.isRealtime" @close="st.isRealtime=false"/>
 </template>
