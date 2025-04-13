193f146805a38d58d8173f144c93c875c65e160f
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2025-04-13 18:52:03 +0800

    Create flow.vue

---
+++ src/views/viggle/flow.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="w-full h-full">
+    <iframe 
+      src="https://flow.nexthubs.cn" 
+      frameborder="0" 
+      class="w-full h-full"
+    ></iframe>
+  </div>
+</template>
