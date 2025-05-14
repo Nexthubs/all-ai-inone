6f19cdc01adf5c69734edcc1a781b231ef503655
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2025-05-14 10:40:40 +0800

    Update zh-CN.ts

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -24,8 +24,8 @@ export default {
     wrong: '好像出错了，请稍后再试。',
     success: '操作成功',
     failed: '操作失败',
-    verify: '验证',
-    unauthorizedTips: '未经授权，请先进行验证。',
+    verify: '登录验证',
+    unauthorizedTips: '尚未登录，请输入密码进行登录验证。',
     stopResponding: '停止响应',
   },
   chat: {
