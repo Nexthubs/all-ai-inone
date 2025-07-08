2a3392bf8cb6111e665b48afb1e109ae69262a10
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2025-07-08 17:33:17 +0800

    Update en-US.ts

--- src/locales/en-US.ts
+++ src/locales/en-US.ts
@@ -25,7 +25,7 @@ export default {
     success: 'Success',
     failed: 'Failed',
     verify: 'Verify',
-    unauthorizedTips: 'Unauthorized, please verify first.',
+    unauthorizedTips: 'Please enter the password to verify first.',
     stopResponding: 'Stop Responding',
   },
   chat: {
