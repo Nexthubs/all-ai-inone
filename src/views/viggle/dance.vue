83c98014f8d36c2fc3f82637bc13eb26dbf9280b
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2025-07-08 18:18:31 +0800

    Update dance.vue

--- src/views/viggle/dance.vue
+++ src/views/viggle/dance.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="w-full h-full">
     <iframe 
-      src="https://flow.nexthubs.cn" 
+      src="https://flow.nexthubs.com" 
       frameborder="0" 
       class="w-full h-full"
     ></iframe>
