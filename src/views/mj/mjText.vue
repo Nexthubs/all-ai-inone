65848665358a83536e767182148639c220608fb0
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2026-05-06 20:07:29 +0800

    Remove otherButton section from mjText.vue
    Removed unnecessary button section for otherButton.

--- src/views/mj/mjText.vue
+++ src/views/mj/mjText.vue
@@ -308,12 +308,7 @@ load();
                     </div>
                     </template>
                 </div>
-                <div v-if="otherButton.length>0" class=" flex justify-start items-center flex-wrap " >
-                    
-                     <div class="p-1"  v-for="ib in otherButton"> 
-                        <NButton  @click="subV2(ib)" size="small" type="info" >{{  ib.emoji??''  }}{{ ib.label??'' }}  </NButton>
-                    </div>
-                </div>
+
 
                 <div class=" flex justify-start items-center flex-wrap " v-if="chat.opt?.action==='UPSCALE' || 'DESCRIBE'===chat.opt?.action"> 
                     <div class="p-1"><NButton  @click="subV3('image')" size="small" type="info" >{{ $t('mj.editImage') }}</NButton></div>
