e9cac3db17a046db9466e255e2c22d0c3b7ff13f
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2026-06-03 15:54:56 +0800

    Add conditional rendering for chat menu item

--- src/views/mj/aiMobileMenu.vue
+++ src/views/mj/aiMobileMenu.vue
@@ -40,7 +40,7 @@ const urouter = useRouter() //
 </script>
 <template>
   <div class=" bg-gray-100 dark:bg-[#282832] h-[55px] flex  justify-around  items-center dark:text-white/70 " >
-      <div class="flex items-center justify-center flex-col"  @click="urouter.push('/chat') && homeStore.setMyData({act:'showChat'}) "   :class="[ goHome =='Chat' ? 'active' : '']" >
+      <div v-if="!isDisableMenu ( 'chat')" class="flex items-center justify-center flex-col"  @click="urouter.push('/chat') && homeStore.setMyData({act:'showChat'}) "   :class="[ goHome =='Chat' ? 'active' : '']" >
         <SvgIcon icon="ri:wechat-line" class="text-3xl"></SvgIcon>
         <div class="text-[13px]">{{$t('mjtab.chat')}}</div>
       </div>
