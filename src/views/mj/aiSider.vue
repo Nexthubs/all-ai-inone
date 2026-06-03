6c335f5c4224a780db0a31053356f1ae82437453
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2026-06-03 10:36:24 +0800

    Change active state from 'chat' to 'dance'

--- src/views/mj/aiSider.vue
+++ src/views/mj/aiSider.vue
@@ -17,7 +17,7 @@ const chatStore = useChatStore()
 const Setting = defineAsyncComponent(() => import('@/components/common/Setting/index.vue'))
 const userStore = useUserStore()
 
-const st= ref({'show':false,showImg:false, menu:[],active:'chat'})
+const st= ref({'show':false,showImg:false, menu:[],active:'dance'})
 
 
 const userInfo = computed(() => userStore.userInfo)
@@ -78,6 +78,19 @@ const handleDanceClick = () => {
     <div class="flex h-full select-none flex-col items-center justify-between bg-[#e8eaf1] px-2 pt-4 pb-8 dark:bg-[#25272d]">
         <div class="flex flex-col space-y-4 flex-1 " :class="{ 'pt-5': homeStore.myData.isClient }" data-tauri-drag-region>
 
+
+            <a v-if="!isDisableMenu ( 'dance')"      @click="st.active='dance'; urouter.push('/dance')" 
+                class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]">
+                <n-tooltip placement="right" trigger="hover">
+                  <template #trigger> 
+                    <div class="flex h-full justify-center items-center py-1 flex-col " :class="[ goHome =='dance' ? 'active' : '']">
+                      <SvgIcon icon="mdi:workflow" class="text-3xl flex-1"></SvgIcon>
+                      <span class="text-[10px]">{{ $t('dance.menu') }}</span>
+                    </div>  
+                  </template>
+                    {{ $t('dance.menuinfo') }}
+                </n-tooltip>                
+            </a>
 
 				<!--   打开新的窗口<a v-if="!isDisableMenu ( 'realtime')"   @click="openExternalLink('https://flow.nexthubs.cn')"  -->
 						
@@ -174,19 +187,6 @@ const handleDanceClick = () => {
                 </n-tooltip>
             </a>
 
-
-            <a v-if="!isDisableMenu ( 'dance')"      @click="st.active='dance'; urouter.push('/dance')" 
-                class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]">
-                <n-tooltip placement="right" trigger="hover">
-                  <template #trigger> 
-                    <div class="flex h-full justify-center items-center py-1 flex-col " :class="[ goHome =='dance' ? 'active' : '']">
-                      <SvgIcon icon="mdi:workflow" class="text-3xl flex-1"></SvgIcon>
-                      <span class="text-[10px]">{{ $t('dance.menu') }}</span>
-                    </div>  
-                  </template>
-                    {{ $t('dance.menuinfo') }}
-                </n-tooltip>                
-            </a>
 
              
 
